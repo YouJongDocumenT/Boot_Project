533ee7fabd1ed59f56ab45f45b5e5a4bbade90d8
YJDocumenT <yjdocument7@gmail.com>
2023-11-17 23:38:53 +0900

    반도 웹 프로젝트 구성

---
+++ Bando_Project/src/main/java/com/bando/project/BandoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bando.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BandoProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BandoProjectApplication.class, args);
+    }
+
+}
---
+++ Bando_Project/src/test/java/com/bando/project/BandoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bando.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BandoProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
