bfb6823bd9dd50af661cadec85121bd73b46d2fc
YJDocumenT <yjdocument7@gmail.com>
2023-12-27 04:28:29 +0900

    웹 템플릿 적용완료 -> 수정 준비중

--- Bando_Web/src/main/java/com/bando/controller/MainPageController.java
+++ Bando_Web/src/main/java/com/bando/controller/MainPageController.java
@@ -9,8 +9,59 @@ public class MainPageController {
 	
 	// 메인화면 매핑
 	@GetMapping("/bando")
-	public String MainPage() {
+	public String Bando() {
 		return "/bando";
 	}
+	
+	// 메인페이지
+	@GetMapping("/main")
+	public String MainPage() throws Exception {
+		
+		return "mainpage/index";
+	}
+	
+	// About페이지(페이지 결정시 이름 변경 필수)
+	@GetMapping("/about")
+	public String AboutPage() throws Exception {
+		
+		return "mainpage/subpage/about";
+	}
+	
+	// courses페이지(페이지 결정시 이름 변경 필수)
+	@GetMapping("/courses")
+	public String CoursesPage() throws Exception {
+
+		return "mainpage/subpage/courses";
+	}
+	
+	// contact페이지(페이지 결정시 이름 변경 필수)
+	@GetMapping("/contact")
+	public String ContactPage() throws Exception {
+		
+		return "mainpage/subpage/contact";
+	}
+
+	// team페이지(페이지 결정시 이름 변경 필수)
+	@GetMapping("/team")
+	public String Team() throws Exception {
+		
+		return "mainpage/subpage/team";
+	}
+	
+	// testimonial페이지(페이지 결정시 이름 변경 필수)
+	@GetMapping("/testimonial")
+	public String Testimonial() throws Exception {
+		
+		return "mainpage/subpage/testimonial";
+	}
+	
+	// 404페이지(페이지 결정시 이름 변경 필수)
+	@GetMapping("/404")
+	public String FzF() throws Exception {
+		
+		return "mainpage/subpage/404";
+	}
+	
+	
 	
 }
