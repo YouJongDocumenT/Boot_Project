53590530d1d788b5bdc34bb5911024ddcffd2e3f
YJDocumenT <yjdocument7@gmail.com>
2023-12-28 23:38:22 +0900

    springframework - security 로그인 remember-me 구현

--- Bando_Web/src/main/java/com/bando/config/LoginSuccessHandler.java
+++ Bando_Web/src/main/java/com/bando/config/LoginSuccessHandler.java
@@ -6,16 +6,24 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
 import org.springframework.security.web.savedrequest.RequestCache;
 import org.springframework.security.web.savedrequest.SavedRequest;
 import org.springframework.stereotype.Service;
 
+import com.bando.dto.UserDetailsDTO;
+
 @Service("loginSuccessHandler")
 public class LoginSuccessHandler implements AuthenticationSuccessHandler{
 
+	//(추가)
+	@Autowired
+	UserDetailsService loginService;
+	
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
 			Authentication authentication) throws IOException, ServletException {
@@ -34,6 +42,11 @@ public class LoginSuccessHandler implements AuthenticationSuccessHandler{
 		if(savedRequest!=null) uri = savedRequest.getRedirectUrl();
 		//경우2 일 경우 uri에 경로 저장
 		else if(prevPage!=null) uri = prevPage;
+		
+		//(추가)
+		UserDetailsDTO userDetailsDto = (UserDetailsDTO) loginService.loadUserByUsername(authentication.getName());
+		userDetailsDto.setPassword(null);
+		
 		response.sendRedirect(uri);
 	}
 }
